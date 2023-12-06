3a2dc91e0a5027091c3d5765cac992ef8e2fda59
Monish <mayekarmonish875@gmail.com>
2023-12-06 14:43:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Contact from '../src/components/Contact'
+import Footer from './components/Footer';
+
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
           <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Footer.css'
+
+function Footer() {
+  return (
+    <div className='footerbar'>
+        Footer
+    </div>
+  )
+}
+
+export default Footer
